3dd047a31f8fdea92cf952ce1431db316343bd5a
OmerOhana <oohana13@gmail.com>
2020-06-07 01:20:13 +0300

    Updates

---
+++ precache-manifest.5d39153a4a6305a73229317ca92540fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86d9b09e506162ada2a05a2e06b5bdf",
+    "url": "/Omer.CV/index.html"
+  },
+  {
+    "revision": "e4acaf00518369db818c",
+    "url": "/Omer.CV/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "78946eede26ba91f30f8",
+    "url": "/Omer.CV/static/css/main.431f63b6.chunk.css"
+  },
+  {
+    "revision": "e4acaf00518369db818c",
+    "url": "/Omer.CV/static/js/2.08c7764b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Omer.CV/static/js/2.08c7764b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78946eede26ba91f30f8",
+    "url": "/Omer.CV/static/js/main.aef3653b.chunk.js"
+  },
+  {
+    "revision": "a6d928b1226fbc0c3d29",
+    "url": "/Omer.CV/static/js/runtime-main.af5bd195.js"
+  },
+  {
+    "revision": "52d6d5136900b3908c52260838a0d30f",
+    "url": "/Omer.CV/static/media/blockchainpic.52d6d513.jpg"
+  },
+  {
+    "revision": "4104592d1e418af0b77309779be31f41",
+    "url": "/Omer.CV/static/media/omer.4104592d.jpg"
+  },
+  {
+    "revision": "952fecac476d93e90eedfdd3f08598f0",
+    "url": "/Omer.CV/static/media/wallpaper.952fecac.jpg"
+  },
+  {
+    "revision": "708e2239be05d90d25ea9ec80b8515e1",
+    "url": "/Omer.CV/static/media/‏‏EarthQuakeWatcher.708e2239.PNG"
+  }
+]);
